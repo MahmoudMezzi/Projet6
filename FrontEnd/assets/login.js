56639e9ebe5e33ba84b1b3971300c7023eeb43ca
Mahmoud <mahmoudmezzi@outlook.fr>
2023-04-14 16:01:32 +0200

    job done

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,34 +1,46 @@
-const button = document.querySelector('.btn');
+const button = document.querySelector(".btn");
 console.log(button);
-const email = document.getElementById('email');
-const password = document.getElementById('psw');
+const email = document.getElementById("email");
+const password = document.getElementById("psw");
+const errorMsg = document.getElementById("error-msg");
 
-button.addEventListener('click', () => {
-  const url = 'http://localhost:5678/api/users/login';
+function displayError(message) {
+  errorMsg.innerText = message;
+  errorMsg.style.display = "block";
+}
+
+button.addEventListener("click", () => {
+  if (!email.value || !password.value) {
+    displayError("Veuillez remplir tous les champs");
+    return;
+  }
+
+  const url = "http://localhost:5678/api/users/login";
   const data = { email: email.value, password: password.value };
   console.log(data);
   fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   })
     .then((response) => {
       console.log(response);
       if (response.ok) {
+        errorMsg.style.display = "none";
         return response.json();
       } else {
-        throw new Error('Identifiant ou mot de passe incorrect');
+        throw new Error("Identifiant ou mot de passe incorrect");
       }
     })
     .then((response) => {
       console.log(response);
-      localStorage.setItem('token', response.token);  
-      window.location.href = './index.html';
+      localStorage.setItem("token", response.token);
+      window.location.href = "./index.html";
+    })
+    .catch((error) => {
+      displayError(error.message);
     });
 });
 
-const modifyBtn = document.getElementById('modify-btn');
-const token = localStorage.getItem('token');
-
-
-
+const modifyBtn = document.getElementById("modify-btn");
+const token = localStorage.getItem("token");
